8ec078bdc5a4bf7a43516ee1f51f1bd526877d35
perubola <fedoboys@hotmail.com>
2024-01-16 00:03:45 -0600

    Messed up last commit, this one has the stuff

--- examples/1000_cells.py
+++ examples/1000_cells.py
@@ -1,7 +1,7 @@
 from culture import culture
 
 test_1000 = culture(num_cells = 1000, m_crit = 10, gr = 1, k=1)
-test_1000.visualize()
+test_1000.viz_distr()
 
 test_1000.sigmoid_grow(2, 30)
-test_1000.visualize()
+test_1000.viz_distr()
--- examples/100_cells.py
+++ examples/100_cells.py
@@ -2,7 +2,7 @@ from culture import culture
 
 test_100 = culture(num_cells = 100, m_crit = 10, gr = 0.5, k=0.2)
 # 0.2 gives log-normal
-test_100.visualize()
+test_100.viz_distr()
 
 test_100.sigmoid_grow(2, 20)
-test_100.visualize()
+test_100.viz_distr()
